60b59e1e3ebf383e23a717e0646934e21d2adff5
jonathantugume569 <jonathantugume569@gmail.com>
2025-02-20 10:53:00 +0300

    a new students app, srializer

---
+++ BACKEND/AITS_backend/student/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from . models import *
+from . serializer import *
+from rest_framework.response import Response
+
+
+class Studentview(APIView):
+    pass
+
+
