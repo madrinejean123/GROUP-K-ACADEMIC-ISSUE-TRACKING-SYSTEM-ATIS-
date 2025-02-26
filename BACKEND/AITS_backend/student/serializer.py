523e4833227372b3c05a3c889c03d08cf63b410d
jonathantugume569 <jonathantugume569@gmail.com>
2025-02-26 12:43:18 +0300

    mmultiple app

--- BACKEND/AITS_backend/student/serializer.py
+++ BACKEND/AITS_backend/student/serializer.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from . models import *
 
 
-class StudentSerializer(serializers.Modelserializer):
+class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ['name', 'student_reg_no', 'student_no', 'email'] 
