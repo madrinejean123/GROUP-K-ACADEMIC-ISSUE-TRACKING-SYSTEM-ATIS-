524bc5d9d6b17e133be9d2ffe93d5d36444818c5
jonathantugume569 <jonathantugume569@gmail.com>
2025-02-26 16:38:29 +0300

    issues

--- BACK_END/AITS_backendproject/AITS_backendproject/settings.py
+++ BACK_END/AITS_backendproject/AITS_backendproject/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'department',
+    'issues',
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
