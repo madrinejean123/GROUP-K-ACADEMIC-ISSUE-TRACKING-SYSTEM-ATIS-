dd6bc8f6da8b9ef3c4d0cf1a010f9a380edcba09
madrinejean123 <jeanmadrinenamulinde@gmail.com>
2025-02-26 15:29:22 +0300

    added

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,24 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Updated import
-import Login from "./Authentication/login.js";
-import Signup from "./Authentication/signup.jsx";
+import Login from "./Pages/LoginPage/login.jsx";
+import Signup from "./Pages/SignupPage/signup.jsx";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Routes>
-          {/* Routing for Home */}
-          <Route path="/" element={<Home />} />
-
-          {/* Routing for Login */}
-          <Route path="/login" element={<Login />} />
-
-          {/* Routing for Signup */}
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <Login />
+      <Signup />
+    </div>
   );
 }
 
