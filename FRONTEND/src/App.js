d575216c7f8d47881922d63b67c5874f285b537e
madrinejean123 <jeanmadrinenamulinde@gmail.com>
2025-02-27 08:05:07 +0300

    Added signup and login screens

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -1,20 +1,21 @@
+
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HeroPage from "./Pages/WelcomePage/HeroPage";
-
+import Signup from "./Pages/SignupPage/signup";
+import Login from "./Pages/LoginPage/login" ;
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          {/* <Route path="/" element={<Footer />} /> */}
           <Route path="/" element={<HeroPage />} />
-          {/* <Route path="/" element={<SignupPage />} /> */}
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element = {<Login />} />
+
         </Routes>
       </Router>
-      {/* <Login /> */}
-      {/* <Signup /> */}
     </div>
   );
 }
