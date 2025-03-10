0bb93a6d3b96a259f896974276a32c49effba9a1
JobkWasswa <jobkwasswa@gmail.com>
2025-03-10 16:41:40 +0300

    added user-dashboards

---
+++ FRONTEND/src/Pages/LecturerDashboard/LecturerDashboard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./LecturerDashboard.css";
+import UserHeader from "../../Components/UserHeader/UserHeader";
+import Footer from "../../Components/Footer/Footer";
+
+const LecturerDashboard = () => {
+  return (
+    <>
+      <UserHeader />
+      <main>
+        <p>Lecturer Dashboard</p>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default LecturerDashboard;
---
+++ FRONTEND/src/Pages/StudentDashboard/StudentDashboard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./StudentDashboard.css";
+import UserHeader from "../../Components/UserHeader/UserHeader";
+import Footer from "../../Components/Footer/Footer";
+
+const StudentDashboard = () => {
+  return (
+    <>
+      <UserHeader />
+      <main>
+        <p>Student Dashboard</p>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default StudentDashboard;
