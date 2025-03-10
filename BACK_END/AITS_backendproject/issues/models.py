01db5b16fe972dc9666667a01b0871e618cf7208
FidelJones <leftoversme@gmail.com>
2025-03-10 14:28:44 +0300

    Created serializer.py file and modified user models

--- BACK_END/AITS_backendproject/issues/models.py
+++ BACK_END/AITS_backendproject/issues/models.py
@@ -12,7 +12,7 @@ class Issues(models.Model):
     status = models.CharField(max_length=20, choices=STATUS_CHOICE, default='pending')
     description = models.TextField()
     attachment = models.ImageField(upload_to='insert tab/', blank=True, null=True)
-    assigned_lecturer = models.CharField(max_length=100)
+    assigned_lecturer = models.CharField(max_length=100, )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
