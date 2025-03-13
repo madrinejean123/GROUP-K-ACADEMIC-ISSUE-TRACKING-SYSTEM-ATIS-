1829fb4bf30df7736a564bdfd1fcc262f2b18208
FidelJones <leftoversme@gmail.com>
2025-03-13 09:37:47 +0300

    i have added the serializers i am going to create the user api s and also fixed some error in the settings REST FRAME

--- BACK_END/AITS_backendproject/AITS_backendproject/settings.py
+++ BACK_END/AITS_backendproject/AITS_backendproject/settings.py
@@ -57,7 +57,7 @@ MIDDLEWARE = [
 
 
 REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES':[
-    'rest_framework.permission.AllowAny'
+    'rest_framework.permissions.AllowAny'
 ]}
 
 CORS_ORIGIN_ALLOW_ALL = True
