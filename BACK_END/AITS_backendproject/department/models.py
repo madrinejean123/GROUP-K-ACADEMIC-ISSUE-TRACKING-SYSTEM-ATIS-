5443a5fbcd25bb6a79cc363055b6a4727c21b56b
FidelJones <leftoversme@gmail.com>
2025-03-17 21:17:53 +0300

    updated the departments models and serializers

--- BACK_END/AITS_backendproject/department/models.py
+++ BACK_END/AITS_backendproject/department/models.py
@@ -1,22 +1,34 @@
 from django.db import models
 
+# College Choices
+COLLEGE_CHOICES = [
+    ('cees', 'College of Education and External Studies (CEES)'),
+    ('cocis', 'College of Computing and Information Sciences (COCIS)'),
+    ('cedat', 'College of Engineering, Design, Art and Technology (CEDAT)'),
+    ('chs', 'College of Health Sciences (CHS)'),
+    ('chuss', 'College of Humanities and Social Sciences (CHUSS)'),
+    ('conas', 'College of Natural Sciences (CONAS)'),
+    ('caes', 'College of Agricultural and Environmental Sciences (CAES)'),
+    ('cobams', 'College of Business and Management Sciences (COBAMS)'),
+    ('covab', 'College of Veterinary Medicine, Animal Resources and Biosecurity (COVAB)'),
+]
+
+# College Model
+class College(models.Model):
+    name = models.CharField(max_length=100, choices=COLLEGE_CHOICES, unique=True)
+    code = models.CharField(max_length=10, unique=True, default="COCIS")  # Default value
+
+    def __str__(self):
+        return self.get_name_display()  # Returns the full name of the college
+    
 
 class Department(models.Model):
-    DEPARTMENT_CHOICES = [
-        ('cees', 'CEES'),
-        ('cocis', 'COCIS'),
-        ('cedat', 'CEDAT'),
-        ('chs', 'CHS'),
-        ('chuss', 'CHUSS'),
-        ('conas', 'CONAS'),
-        ('caes', 'CAES'),
-        ('cobams', 'COBAMS'),
-        ('covab', 'COVAB'),
-        ('school of law', 'SCHOOL OF LAW'),
-    ]
-    department_name = models.CharField(max_length=100, verbose_name='Department', choices=DEPARTMENT_CHOICES)
+    name = models.CharField(max_length=100, unique=True, default="Unnamed Department")  # Default value
     description = models.TextField(verbose_name='Department Description')
-    
+    college = models.ForeignKey(College, on_delete=models.CASCADE, related_name='departments', default=1)
+
     def __str__(self):
-        return self.department_name
+        return f"{self.name} ({self.college.name})"
+    
+
     
