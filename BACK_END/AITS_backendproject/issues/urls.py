7d290f7cefdb078c45645bf6952910c82da08c71
jonathantugume569 <jonathantugume569@gmail.com>
2025-03-19 13:22:43 +0300

    Fixed the errors in issues/urls.py

--- BACK_END/AITS_backendproject/issues/urls.py
+++ BACK_END/AITS_backendproject/issues/urls.py
@@ -4,7 +4,7 @@ from .views import IssueView, IssueDetailView, CollegeRegisterIssueView, Student
 
 urlpatterns = [
     path('issues/', IssueView.as_view(), name='issue-list'),
-    path('<issues/<`int:pk>/', IssueDetailView.as_view(), name='issue-detail'),
+    path('<int:pk>/', IssueDetailView.as_view(), name='issue-detail'),
     path('register/issues/', CollegeRegisterIssueView.as_view(), name='register-issues'),
     path('lecturer/issues/', LecturerIssueView.as_view(), name='lecturer-issues'),
     path('student/send-issue/', StudentSendIssueView.as_view(), name='student-send-issue'),
