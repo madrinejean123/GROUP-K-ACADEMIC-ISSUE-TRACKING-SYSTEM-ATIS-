b9654f1372192604cb87146d34f8cfbc2669127a
JobkWasswa <jobkwasswa@gmail.com>
2025-03-23 19:03:18 +0300

    added lecturer dashboard

--- FRONTEND/src/Components/HomeHeader/HomeHeader.js
+++ FRONTEND/src/Components/HomeHeader/HomeHeader.js
@@ -1,9 +1,5 @@
 import React from "react";
 import "./HomeHeader.css";
-import { MdAddCall } from "react-icons/md";
-import { FaSquareXTwitter } from "react-icons/fa6";
-import { FaSquareFacebook } from "react-icons/fa6";
-import { FaSquareWhatsapp } from "react-icons/fa6";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom"; 
 import MakLogo from "../assets/logo.png";
@@ -11,29 +7,6 @@ import MakLogo from "../assets/logo.png";
 const HomeHeader = () => {
   return (
     <header className="header">
-      {/* <div className="upper">
-        <div className="call-us">
-          <p>
-            <MdAddCall />
-            <span>
-              Call Us: <a href="tel">+256-123-456-7890</a>
-            </span>
-          </p>
-        </div>
-
-        <div className="links">
-          <p>REACH US:</p>
-          <a href="">
-            <FaSquareFacebook />
-          </a>
-          <a href="">
-            <FaSquareWhatsapp />
-          </a>
-          <a href="">
-            <FaSquareXTwitter />
-          </a>
-        </div>
-      </div> */}
       <div className="lower">
         <div className="logo">
           <img src={MakLogo} alt="Mak Logo" />
