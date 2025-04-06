ef0b10bfcfe44c353885b19c619deefac48ce6e7
jonathantugume569 <jonathantugume569@gmail.com>
2025-04-06 15:58:01 +0300

    Added admin create urls

--- BACK_END/AITS_backendproject/department/urls.py
+++ BACK_END/AITS_backendproject/department/urls.py
@@ -8,7 +8,9 @@ from .views import (
     StudentSchoolView,
     CollegeSchoolsView,
     SchoolDepartmentView, 
-    
+    CreateCollegeView,
+    CreateSchoolView,
+    CreateDepartmentView,
 )
 
 urlpatterns = [
@@ -18,7 +20,9 @@ urlpatterns = [
     path('student/college/', StudentCollegeView.as_view(), name='student-college'),
     path('student/school/', StudentSchoolView.as_view(), name='student-school'),
     path('lecturer/colleges/', LecturerCollegeView.as_view(), name='lecturer-college'),
-    path('college/schools/',CollegeSchoolsView.as_view(),name='college-schools' ),
-    path('school/departments/', SchoolDepartmentView.as_view(), name='school-departments'),
-   
+    path('college/<int:college_id>/schools/',CollegeSchoolsView.as_view(), name='college-schools' ),
+    path('school/<int:school_id>/departments/', SchoolDepartmentView.as_view(), name='school-departments'),
+    path('admin/create-college/',CreateCollegeView.as_view(), name='create-college'),
+    path('admin/create-school/',CreateSchoolView.as_view(), name='create-school'),
+    path('admin/create-department/',CreateDepartmentView.as_view(), name='create-department'),
 ]
