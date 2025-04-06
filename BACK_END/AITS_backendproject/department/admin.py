a83d153c863adce82a6526422539c1bcc88a1e2e
jonathantugume569 <jonathantugume569@gmail.com>
2025-04-06 23:57:58 +0300

    Modiffied admin.py files in the users and department apps to enable the admin interact accordingly

--- BACK_END/AITS_backendproject/department/admin.py
+++ BACK_END/AITS_backendproject/department/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import College, School, Department
+
+admin.site.register(College)
+admin.site.register(School)
+admin.site.register(Department)
+
+
